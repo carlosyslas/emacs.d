ed127e9bd8ec6c9493e660febea35b34d9e58e28
cyslas <cyslas@switchfly.com>
2014-04-07 19:48:40 -0700

    Added package repositories

--- init.el
+++ init.el
@@ -1,6 +1,11 @@
 (add-to-list 'load-path user-emacs-directory) ;; ~/.emacs.d
 (setq is-mac (equal system-type 'darwin))
 
+;; Repositories
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
 ;; Keep emacs Custom-settings in separate file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
@@ -13,3 +18,5 @@
   (require 'setup-mac-osx)) 
 (require 'setup-key-bindings)
 ;; End of init.el
+
+
