1a88ca2f0f390d1b0a7e4ec1d4ee58e5b81ad402
Carlos Yslas <carlos.yslasa@gmail.com>
2018-05-29 18:14:15 +0200

    Install from homebrew

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 # NOTE: this script is ment to be used by
 # https://github.com/carlosyslas/dot-files
 
+brew install emacs
+
 if [[ -d ~/.emacs.d && ! -L ~/.emacs.d ]]; then
     rm -rf ~/.emacs.d
     ln -s $INSTALL_DIR/emacs.d ~/.emacs.d
