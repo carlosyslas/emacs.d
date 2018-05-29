09a2b28a21eda88c3ecc7cbc513213f60c51dd91
Carlos Yslas <carlos.yslasa@gmail.com>
2018-05-29 17:52:59 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# NOTE: this script is ment to be used by
+# https://github.com/carlosyslas/dot-files
+
+if [[ -d ~/.emacs.d && ! -L ~/.emacs.d ]]; then
+    rm -rf ~/.emacs.d
+    ln -s $INSTALL_DIR/emacs.d ~/.emacs.d
+fi
