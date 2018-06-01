0a55f99bd9d4375f5bb8dd79455ead50ad03f48d
Carlos Yslas <carlos.yslasa@gmail.com>
2018-06-01 14:26:44 +0200

    Add back previously removed files

--- self/common.el
+++ self/common.el
@@ -1,1 +1,39 @@
+(setq is-mac (equal system-type 'darwin))
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(pending-delete-mode t)
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+(when is-mac
+  (add-hook 'emacs-startup-hook 'toggle-frame-maximized))
+
+(setq warning-minimum-level :error)
+
+(setq initial-scratch-message nil)
+
+(setq inhibit-startup-message t)
+
+(menu-bar-mode -1)
+
+(if (boundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+
+(if (boundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+(when is-mac
+  ;; Set option to nil to enable extrange chars
+  (setq mac-option-modifier nil)
+  ;; Use control from Meta
+  (setq ns-command-modifier (quote meta))
+  ;; Set right option to control
+  (setq mac-right-option-modifier 'control))
+
 (provide 'common)
--- self/programing.el
+++ self/programing.el
@@ -1,1 +1,19 @@
+(setq self/indentation-spaces 2)
+
+(global-subword-mode)
+
+(show-paren-mode t)
+
+(global-hl-line-mode)
+
+(setq js2-basic-offset self/indentation-spaces)
+(setq js2-bounce-indent-p nil)
+(setq js-indent-level self/indentation-spaces
+      indent-tabs-mode nil
+      c-basic-offset self/indentation-spaces)
+(setq css-indent-offset self/indentation-spaces)
+(setq-default indent-tabs-mode nil)
+(setq indent-tabs-mode nil)
+(setq python-indent-offset self/indentation-spaces)
+
 (provide 'programing)
