baadf054e26607ff963503165035050e0d6d8d0b
Carlos Yslas <cyslas@newstore.com>
2018-10-17 15:58:46 +0200

    Migrate Mac OS config to literate style

--- self/common.el
+++ self/common.el
@@ -1,15 +1,10 @@
 (require 'uniquify)
 
-(setq is-mac (equal system-type 'darwin))
-
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (pending-delete-mode t)
 
 (add-hook 'before-save-hook 'whitespace-cleanup)
-
-(when is-mac
-  (add-hook 'emacs-startup-hook 'toggle-frame-maximized))
 
 (setq warning-minimum-level :error)
 
@@ -29,14 +24,6 @@
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
-
-(when is-mac
-  ;; Set option to nil to enable extrange chars
-  (setq mac-option-modifier nil)
-  ;; Use control from Meta
-  (setq ns-command-modifier (quote meta))
-  ;; Set right option to control
-  (setq mac-right-option-modifier 'control))
 
 (setq uniquify-buffer-name-style 'forward)
 (setq uniquify-separator "/")
